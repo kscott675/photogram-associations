1834ed326e18c44afca3661cca75eac7ce96618b
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-02-20 16:33:26 +0000

    completed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -29,11 +29,11 @@ class Photo < ApplicationRecord
 
   # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
 
-  #has_many(:likes, class_name: "Like")
+  has_many(:likes, class_name: "Like", foreign_key: "photo_id")
 
   ## Indirect associations
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
 
-  has_many(:fans, through: :likes, source: :user)
+  has_many(:fans, through: :likes, source: :fan)
 end
